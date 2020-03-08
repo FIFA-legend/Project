0935e44b0e3e8113338e0f328f67105e81a439c5
FIFA-legend <58667610+FIFA-legend@users.noreply.github.com>
2020-03-08 21:08:58 +0300

    Delete Main.java

--- src/by/itcollege/Main.java
+++
@@ -1,9 +0,0 @@
-package by.itcollege;
-
-import by.itcollege.entity.CarType;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(CarType.BUSINESS.toString());
-    }
-}
